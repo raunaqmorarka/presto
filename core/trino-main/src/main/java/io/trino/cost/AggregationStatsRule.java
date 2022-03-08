14f0a5a755dc74d8d5b46fa5ad636f5380ebcb3f
Raunaq Morarka <raunaqmorarka@gmail.com>
2022-03-08 16:58:20 +0530

    Add aggregate symbol stats for min/max/sum/count

--- core/trino-main/src/main/java/io/trino/cost/AggregationStatsRule.java
+++ core/trino-main/src/main/java/io/trino/cost/AggregationStatsRule.java
@@ -20,11 +20,13 @@ import io.trino.sql.planner.TypeProvider;
 import io.trino.sql.planner.iterative.Lookup;
 import io.trino.sql.planner.plan.AggregationNode;
 import io.trino.sql.planner.plan.AggregationNode.Aggregation;
+import io.trino.sql.tree.SymbolReference;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
 
+import static io.trino.operator.scalar.MathFunctions.isNaN;
 import static io.trino.sql.planner.plan.AggregationNode.Step.SINGLE;
 import static io.trino.sql.planner.plan.Patterns.aggregation;
 import static java.lang.Math.min;
@@ -76,11 +78,16 @@ public class AggregationStatsRule
             }));
         }
 
-        double rowsCount = getRowsCount(sourceStats, groupBySymbols);
-        result.setOutputRowCount(min(rowsCount, sourceStats.getOutputRowCount()));
+        double rowsCount = min(getRowsCount(sourceStats, groupBySymbols), sourceStats.getOutputRowCount());
+        result.setOutputRowCount(rowsCount);
 
         for (Map.Entry<Symbol, Aggregation> aggregationEntry : aggregations.entrySet()) {
-            result.addSymbolStatistics(aggregationEntry.getKey(), estimateAggregationStats(aggregationEntry.getValue(), sourceStats));
+            // Calculate aggregate stats only when the number of grouping symbols is zero or one.
+            // Chances of over-estimation are high with multiple grouping columns due to multiplication of NDVs without
+            // taking into account the correlation in the data of the columns.
+            if (groupBySymbols.size() <= 1) {
+                result.addSymbolStatistics(aggregationEntry.getKey(), estimateAggregationStats(aggregationEntry.getValue(), sourceStats, rowsCount));
+            }
         }
 
         return result.build();
@@ -97,12 +104,78 @@ public class AggregationStatsRule
         return rowsCount;
     }
 
-    private static SymbolStatsEstimate estimateAggregationStats(Aggregation aggregation, PlanNodeStatsEstimate sourceStats)
+    private static SymbolStatsEstimate estimateAggregationStats(Aggregation aggregation, PlanNodeStatsEstimate sourceStats, double aggregatedRowCount)
     {
         requireNonNull(aggregation, "aggregation is null");
         requireNonNull(sourceStats, "sourceStats is null");
 
-        // TODO implement simple aggregations like: min, max, count, sum
-        return SymbolStatsEstimate.unknown();
+        if (aggregation.getArguments().size() != 1
+                || !(aggregation.getArguments().get(0) instanceof SymbolReference)
+                || aggregation.isDistinct()
+                || aggregation.getFilter().isPresent()
+                || aggregation.getMask().isPresent()
+                || isNaN(aggregatedRowCount)) {
+            // Attempt to estimate only simple aggregations
+            return SymbolStatsEstimate.unknown();
+        }
+        if (aggregatedRowCount == 0) {
+            return SymbolStatsEstimate.zero();
+        }
+        Symbol aggregateSymbol = Symbol.from(aggregation.getArguments().get(0));
+        SymbolStatsEstimate aggregatedSymbolSourceStats = sourceStats.getSymbolStatistics(aggregateSymbol);
+        if (aggregatedSymbolSourceStats.isUnknown()) {
+            return SymbolStatsEstimate.unknown();
+        }
+        SymbolStatsEstimate.Builder builder = SymbolStatsEstimate.buildFrom(aggregatedSymbolSourceStats)
+                .setDistinctValuesCount(aggregatedRowCount);
+        switch (aggregation.getResolvedFunction().getSignature().getName()) {
+            case "min":
+                if (aggregatedRowCount == 1) {
+                    builder.setLowValue(aggregatedSymbolSourceStats.getLowValue())
+                            .setHighValue(aggregatedSymbolSourceStats.getLowValue());
+                }
+                break;
+            case "max":
+                if (aggregatedRowCount == 1) {
+                    builder.setLowValue(aggregatedSymbolSourceStats.getHighValue())
+                            .setHighValue(aggregatedSymbolSourceStats.getHighValue());
+                }
+                break;
+            case "sum":
+                if (aggregatedRowCount == 1) {
+                    return SymbolStatsEstimate.unknown();
+                }
+                else {
+                    double avgRowsPerGroup = sourceStats.getOutputRowCount() / aggregatedRowCount;
+                    // We don't know how skewed the data distribution is over the groupBy symbols.
+                    // We take the avgRowsPerGroup and multiply it by 10 to come up with some upper bound for group size.
+                    double approxMaxRowsPerGroup = min(10 * avgRowsPerGroup, aggregatedRowCount);
+                    double sourceLowValue = aggregatedSymbolSourceStats.getLowValue();
+                    double sourceHighValue = aggregatedSymbolSourceStats.getHighValue();
+                    // Extrapolate a lower/upper bound for low/high values in the skewed group
+                    builder.setLowValue(sourceLowValue < 0 ? sourceLowValue * approxMaxRowsPerGroup : sourceLowValue)
+                            .setHighValue(sourceHighValue < 0 ? sourceHighValue : sourceHighValue * approxMaxRowsPerGroup);
+                }
+                break;
+            case "count":
+                if (aggregatedRowCount == 1) {
+                    builder.setLowValue(sourceStats.getOutputRowCount())
+                            .setHighValue(sourceStats.getOutputRowCount());
+                }
+                else if (sourceStats.getOutputRowCount() == aggregatedRowCount) {
+                    builder.setLowValue(1)
+                            .setHighValue(1)
+                            .setDistinctValuesCount(1);
+                }
+                else {
+                    // Skew in data distribution over the groupBy symbols is unknown
+                    return SymbolStatsEstimate.unknown();
+                }
+                break;
+            default:
+                // Estimating aggregations other than min, max, count, sum is not supported
+                return SymbolStatsEstimate.unknown();
+        }
+        return builder.build();
     }
 }
